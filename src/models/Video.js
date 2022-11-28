48776f1c7e0d393e140dba7605eb28e80930b499
Tei <dy0447808@gmail.com>
2022-11-28 20:24:43 +0900

    Join & Login

--- src/models/Video.js
+++ src/models/Video.js
@@ -4,12 +4,17 @@ const videoSchema = new mongoose.Schema({
     title: {type: String, required: true, minLength:2, maxLength:25},
     description: {type: String, required: true, maxLength:300},
     createdAt: {type: Date, required: true, default: Date.now},
-    hashtags: [{type: String}],
+    hashtags: [{type: String, trim: true}],
     meta: {
         views:{type:Number, default: 0, requried: true},
         rating:{type:Number, default: 0, requried: true},
     },
 });
 
+videoSchema.static('formatHashtags', function(hashtags) {
+    return hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
+})
+
 const Video = mongoose.model("Video", videoSchema);
+
 export default Video;
