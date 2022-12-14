0b7f334ea47c69ee2d878d2206c3f385303d2582
Tei <dy0447808@gmail.com>
2022-12-14 11:01:20 +0900

    edit

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,13 +1,19 @@
 import express from "express";
-import {edit, remove, seeUser, logout, startGithubLogin, finishGithubLogin} from "../controllers/userController";
+import {
+    getEdit,
+    postEdit,
+    seeUser,
+    logout,
+    startGithubLogin, 
+    finishGithubLogin} from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddlewware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddlewware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddlewware, finishGithubLogin);
 userRouter.get(":id", seeUser);
 
 export default userRouter;
